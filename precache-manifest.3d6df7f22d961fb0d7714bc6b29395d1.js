514e82c034e1635566be606161508acf8271308f
Vladislav V <0428vlad0428@mail.ru>
2021-10-26 17:30:45 +0300

    Updates

---
+++ precache-manifest.3d6df7f22d961fb0d7714bc6b29395d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e03f4a6aaa58f3106cc9c259478632",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "46ffb519457d48c14125",
+    "url": "/todoList/static/css/main.a9f2e412.chunk.css"
+  },
+  {
+    "revision": "a09b12bfe75711899df3",
+    "url": "/todoList/static/js/2.14447ee8.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todoList/static/js/2.14447ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ffb519457d48c14125",
+    "url": "/todoList/static/js/main.bb8cba7d.chunk.js"
+  },
+  {
+    "revision": "c39c464575b971d22f9a",
+    "url": "/todoList/static/js/runtime-main.67bcfed6.js"
+  }
+]);
